7908e2aad8bf3288a5910f1eaf3073e8cd6d0a67
vmwavie <vmwavie@protonmail.com>
2025-01-09 17:52:35 -0300

    teste

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/webhook", (req, res) => {
+  try {
+    const notification = req.body;
+
+    if (notification.charges && notification.charges.length > 0) {
+      const charge = notification.charges[0];
+      const paymentType = charge.payment_method?.type || "UNKNOWN";
+
+      console.log("Tipo de pagamento:", paymentType);
+    } else {
+      console.log("Cobranças não encontradas no payload.");
+    }
+
+    res.status(200).send("Notification received");
+  } catch (error) {
+    console.error("Erro ao processar a notificação:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(8080, () => {
+  console.log(`Servidor rodando na porta ${8080}`);
+});
